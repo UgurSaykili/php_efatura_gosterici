fdc005f49dba45f93642fc6636ec2a6e442ee005
Murat Karagöz <muratkaragoz@users.noreply.github.com>
2019-08-13 14:32:20 +0300

    Update index.php

--- sertifika_inceleme_demo/index.php
+++ sertifika_inceleme_demo/index.php
@@ -1,9 +1,20 @@
 <?php
 
-	   $FaturaXmlDosyasi=file_get_contents( dirname( __FILE__ )."/fatura.xml" );
+	        $FaturaXmlDosyasi=file_get_contents( dirname( __FILE__ )."/fatura.xml" );
 	
 		include 'efaturagoster.php';
-    $FaturaSertifikaBilgisi=FaturaSertifikaBilgisiOlustur($FaturaXmlDosyasi);
+                $FaturaSertifikaBilgisi=FaturaSertifikaBilgisiOlustur($FaturaXmlDosyasi);
 
 		echo $FaturaSertifikaBilgisi;
+
+
+
+/******************************    Sertifika Kaydet     ***********************************/	
+            $SertifikaDosyaAdiPEM = dirname( __FILE__ )."/sertifika.pem";
+            SertifikaKaydetPEM($FaturaXmlDosyasi,$SertifikaDosyaAdiPEM);
+		
+	    $SertifikaDosyaAdiDER = dirname( __FILE__ )."/sertifika.der";
+            SertifikaKaydetDER($FaturaXmlDosyasi,$SertifikaDosyaAdiDER);
+
+
 ?>
